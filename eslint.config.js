55a422ec1c94978e11e3ef0776380d329465e6d7
Rodion Lvb <rodionlvb17@gmail.com>
2026-03-31 14:29:21 +0300

    step 4: configure eslint and fix code style

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import globals from 'globals';
+import js from '@eslint/js';
+
+export default [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  js.configs.recommended,
+  {
+    rules: {
+      'no-console': 'off',
+    },
+  },
+];
